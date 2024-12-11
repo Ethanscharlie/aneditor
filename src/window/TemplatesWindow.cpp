f8ee03273f12ed8bc11b6c1cefe86973f838e99c
Ethanscharlie <ethanscharlie@protonmail.com>
2024-12-10 23:39:09 -0700

    You can add and remove properties

--- src/window/TemplatesWindow.cpp
+++ src/window/TemplatesWindow.cpp
@@ -6,9 +6,13 @@
 #include <SDL_blendmode.h>
 #include <SDL_rect.h>
 #include <SDL_render.h>
+#include <any>
+#include <cstdint>
 #include <format>
 #include <iostream>
 #include <map>
+#include <memory>
+#include <pstl/glue_algorithm_defs.h>
 #include <string>
 #include <vector>
 
@@ -16,15 +20,50 @@
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_sdlrenderer2.h"
 
+enum class PropertyType {
+  Decimal = 0,
+  Integer = 1,
+  Checkbox = 2,
+  Text = 3,
+  Filepath = 4,
+  Color = 5
+};
+
+static float colorTest[3] = {255, 255, 255};
+
 struct Property {
+  Property(PropertyType type) : type(type) {
+    switch (type) {
+    case PropertyType::Decimal:
+      value = (float)0.0;
+      break;
+    case PropertyType::Integer:
+      value = (int)0;
+      break;
+    case PropertyType::Checkbox:
+      value = false;
+      break;
+    case PropertyType::Text:
+      value = (std::string) "";
+      break;
+    case PropertyType::Filepath:
+      value = (std::string) "";
+      break;
+    case PropertyType::Color:
+      value = std::array<float, 3>({255, 255, 255});
+      break;
+    }
+  }
+
   std::string name = "";
-  int value = 0;
+  const PropertyType type;
+  std::any value;
 };
 
 struct Template {
   Template(std::string name) : name(name) {}
   std::string name;
-  std::vector<Property> properties;
+  std::vector<Property *> properties;
 };
 
 static int selectedTemplate;
@@ -56,19 +95,77 @@ void templatesWindow() {
       ImGui::Separator();
 
       ImGui::Text("Properties");
-      for (int i = 0; i < templates[selectedTemplate].properties.size(); i++) {
-        Property &property = templates[selectedTemplate].properties[i];
+      auto &properties = templates[selectedTemplate].properties;
 
+      for (int i = 0; i < properties.size(); i++) {
         ImGui::TableNextColumn();
+        Property &property = *properties[i];
+
+        if (ImGui::Button(std::format("-###RemoveButton{}", i).c_str())) {
+          std::vector<Property *>::iterator it = properties.begin() + i;
+          properties.erase(it);
+          std::cout << std::format("{}\n", properties.size());
+          delete *it;
+          i--;
+          continue;
+        }
+
+        ImGui::SameLine();
+
+        ImGui::SetNextItemWidth(100.0f);
         ImGui::InputString(std::format("###String Input {}", i).c_str(),
                            &property.name, 128, ImGuiInputTextFlags_None);
+
         ImGui::SameLine();
-        ImGui::InputInt(std::format("###PropValue{}", i).c_str(),
-                        &property.value);
+
+        switch (property.type) {
+        case PropertyType::Decimal: {
+          float &value = std::any_cast<float &>(property.value);
+          ImGui::InputFloat(std::format("###PropValue{}", i).c_str(), &value);
+          break;
+        }
+        case PropertyType::Integer: {
+          ImGui::InputInt(std::format("###PropValue{}", i).c_str(),
+                          &std::any_cast<int &>(property.value));
+          break;
+        }
+        case PropertyType::Checkbox: {
+          ImGui::Checkbox(std::format("###PropValueIn{}", i).c_str(),
+                          &std::any_cast<bool &>(property.value));
+          break;
+        }
+        case PropertyType::Text: {
+          ImGui::InputString(std::format("###String InputIn {}", i).c_str(),
+                             &std::any_cast<std::string &>(property.value), 128,
+                             ImGuiInputTextFlags_None);
+          break;
+        }
+        case PropertyType::Filepath: {
+          ImGui::InputString(std::format("###String InputIn {}", i).c_str(),
+                             &std::any_cast<std::string &>(property.value), 128,
+                             ImGuiInputTextFlags_None);
+          break;
+        }
+        case PropertyType::Color: {
+          auto &color = std::any_cast<std::array<float, 3> &>(property.value);
+          ImGui::ColorEdit3(std::format("###Color Input {}", i).c_str(),
+                            color.data());
+          break;
+        }
+        }
       }
 
-      if (ImGui::Button("         +         ")) {
-        templates[selectedTemplate].properties.push_back(Property());
+      int dropdownSelection = 0;
+      std::array<const char *, 7> dropdownOptions = {
+          "Add a Property", "Decimal",  "Integer", "Checkbox",
+          "Text",           "Filepath", "Color"};
+      if (ImGui::Combo("### Add prop dropdown", &dropdownSelection,
+                       dropdownOptions.data(), 7)) {
+        if (dropdownSelection != 0) {
+          Property *newProperty =
+              new Property((PropertyType)(dropdownSelection - 1));
+          templates[selectedTemplate].properties.push_back(newProperty);
+        }
       }
 
       ImGui::Separator();
